76e0d47d8f9330c757b0a105553604eab80c3786
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2020-01-26 13:35:30 +0900

    item_view_show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def cart_create
+    cartin_item = {n: cart_params[:name], p: cart_params[:price], q: cart_params[:quantity]}.stringify_keys
+    if session[:cartitems]
+      session[:cartitems] << cartin_item
+    else
+      session[:cartitems] = []
+      session[:cartitems] << cartin_item
+    end
+    # @cart = session[:cartitems]
+  end
+
+  def cart_params
+    params.permit(:code, :name, :price, :quantity)
+  end
+
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,8 @@ class CartsController < ApplicationController
     @cart = session[:cartitems]
     redirect_back fallback_location: { action: "items" }, notice: 'カートに追加しました。'
   end
+
+  # def cart_params
+  #   params.permit(:code, :name, :price, :quantity)
+  # end
 end
