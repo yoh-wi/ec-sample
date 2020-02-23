0dea1b4216ff43087d0f9d81d16a5db23aedd9bc
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2020-02-23 16:15:46 +0900

    modify_ordernew_view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,17 @@
 class Order < ApplicationRecord
   has_many :orderitem, dependent: :destroy
   belongs_to :user
+
+  # def oreder_sum(tax)
+  #   tax0sum = 0
+  #   tax8sum = 0
+  #   tax10sum = 0
+  #   itempqsum = 0
+  #   @cart.each do |item|
+  #     p = item.itemprice
+  #     q = item.quantity
+  #     itempq = p * q
+  #     tax = item.item.taxrate
+
+
 end
