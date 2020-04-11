169437e2b270f846f2400b7b47e5faf72d7d28b2
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2020-04-12 00:57:00 +0900

    delete_migrationfile

--- db/migrate/620200131044357_change_data_item_id_to_orderitems.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataItemIdToOrderitems < ActiveRecord::Migration[5.2]
-  def change
-    change_column :orderitems, :item_id, :integer
-  end
-end
--- db/migrate/620200131044358_delete_item_id_index_from_orderitems.rb
+++
@@ -1,5 +0,0 @@
-class DeleteItemIdIndexFromOrderitems < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :orderitems, :item_id
-  end
-end
