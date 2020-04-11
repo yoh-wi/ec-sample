3662c7dbdea5a4e1a06951c570779e829eb06995
yoh-wi <57764555+yoh-wi@users.noreply.github.com>
2020-04-11 15:37:07 +0900

    modify_itemshow

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,11 @@
+// $(function() {
+//   function buildImg(index, url){
+//     var imgHtml = `<div class='js-preview' data-index="${index}">
+//                     <div class='js-img'>
+//                       <img data-index="${index}" src="${url}" width="120px" height="100px">
+//                     </div>
+//                     <div class="js-remove">削除</div>
+//                    </div>`;
+//     return imgHtml
+//   }
+// })
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,11 @@ class Item < ApplicationRecord
   belongs_to :category, optional: true
   has_many :orderitems
 
+  validates :category, presence: true
   validates :code, presence: true, uniqueness: true, length: { maximum:13 } 
   validates :name, presence: true
   validates :price, presence: true
+  validates :taxrate, presence: true
 
   mount_uploader :image, ImageUploader
 end
